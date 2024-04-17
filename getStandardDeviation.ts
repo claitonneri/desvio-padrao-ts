9a0fd9cac3365f765b5963a592d0c299b7b1a820
Claiton Neri <claitondidone@gmail.com>
2024-04-17 11:31:17 +0000

    chore: funcao desvio padrao

---
+++ getStandardDeviation.ts
@@ -0,0 +1,8 @@
+export default function getStandardDeviation(array: number[]): number {
+    const n = array.length
+    const mean = array.reduce((a, b) => a + b) / n
+    return Math.sqrt(
+      array.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n,
+    )
+  }
+  
